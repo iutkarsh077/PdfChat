25a4544a428354749b7001f82b339f3d40879db9
utkarsh <utkarshsingh132003@gmail.com>
2026-01-01 11:42:35 +0530

    model changed

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,13 +2,13 @@
 import { TaskType } from "@google/generative-ai";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { QdrantVectorStore } from "@langchain/qdrant";
-import OpenAI from "openai";
 import { NextResponse } from "next/server";
+import { GoogleGenAI, ThinkingLevel } from "@google/genai";
 
-const openai = new OpenAI({
-  apiKey: process.env.GOOGLE_API_KEY,
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY
 });
+
 
 export async function POST(req: Request) {
   try {
@@ -32,24 +32,28 @@ export async function POST(req: Request) {
 
     const relevantChunks = await vectorRetriver.invoke(msg);
 
+    // console.log(relevantChunks)
+
     const SYSTEM_PROMPT = `You are an AI assistant that answers questions based on the provided context available to you from a file with the content and page number.
   Only answer based on the available context from file and also give page Number.
   
   Context:
   ${JSON.stringify(relevantChunks)}`;
 
-  const response = await openai.chat.completions.create({
-    model: "gemini-2.0-flash",
-    messages: [
-      { role: "system", content: SYSTEM_PROMPT },
-      {
-        role: "user",
-        content: msg,
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: msg,
+    config: {
+      systemInstruction: SYSTEM_PROMPT,
+      thinkingConfig: {
+        thinkingBudget: 400
       },
-    ],
+      maxOutputTokens: 1024,
+      temperature: 0.7
+    }
   });
 
-  const result = response.choices[0].message.content;
+  const result = response.text;
   // console.log("Result of RAG is: ", result);
 
   return NextResponse.json({message: "Success", data: result, status: true});
